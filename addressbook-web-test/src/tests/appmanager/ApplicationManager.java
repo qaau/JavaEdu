ce0a5e285187e74e2f7f327e23b619525d1aacd7
vitaliy <70757477+qaau@users.noreply.github.com>
2020-10-19 22:08:17 +0300

    adding browser variation to our test

--- addressbook-web-test/src/tests/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/tests/appmanager/ApplicationManager.java
@@ -2,6 +2,8 @@ package tests.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,9 +12,18 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  public String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    driver = new ChromeDriver();
+    if (browser == BrowserType.CHROME) {
+      driver = new ChromeDriver();
+    }  else if (browser == BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    }
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/index.php");
     groupHelper = new GroupHelper(driver);
