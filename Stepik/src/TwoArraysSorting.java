a7da6fd9cfa00fd8ce2c021675d903d383c26c31
vitaliy <70757477+qaau@users.noreply.github.com>
2020-12-02 15:55:44 +0200

    Sorting and other stuff

---
+++ Stepik/src/TwoArraysSorting.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TwoArraysSorting {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    if (n < 1 || n > 1000) return;
+    int[] a = new int[n];
+    String count = "";
+
+    for (int i = 0; i < n; i++) {
+      a[i] = in.nextInt();
+    }
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < n - 1; j++) {
+        if (a[j] > a[j + 1]) {
+          int buf = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = buf;
+        }
+      }
+    }
+    for (int i : a) System.out.print(i + " ");
+  }
+}
+
+
